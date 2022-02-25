8350e9f279eeb8f59fdcbf267a5b9e7b971a17cf
yoonserver <yoonserver@gmail.com>
2022-02-26 00:03:41 +0900

    HelloController 코드를 롬복으로 전환

--- src/test/java/com/yoonserver/study/springboot/web/HelloControllerTest.java
+++ src/test/java/com/yoonserver/study/springboot/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.yoonserver.study.springboot.web;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,5 +25,16 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
             .andExpect(status().isOk())
             .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is(name)))
+            .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
